0ff796e8f23813b062401e807b5ed11cbb30d78a
lyiuherman <leeronsajesh123@gmail.com>
2024-03-30 10:46:15 +0530

    commited otp

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,7 +8,7 @@ const {
     getmyaccount,
     getmyOrder,
     getwishlist,
-    verifyotp
+   
 }= require("../controller/userController")
 const UserOTPverification = require("../model/otp")
 
@@ -20,7 +20,6 @@ router.post("/signup",postsignup);
 router.get("/myAccount",getmyaccount);
 router.get("/myOrder",getmyOrder);
 router.get("/wishlist",getwishlist);
-router.post("/verify-otp",verifyotp)
 
 module.exports = router
 
