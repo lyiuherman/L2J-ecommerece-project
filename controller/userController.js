99bc4a437d4db7307e3ece566d5686320f530083
lyiuherman <leeronsajesh123@gmail.com>
2024-04-02 11:31:53 +0530

    veryfing email verification

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,7 @@
 const UserOTPverification = require("../model/otp");
 const User = require("../model/user");
-const nodemailer = require("nodemailer")
-const bcrypt = require('bcrypt');
+const nodemailer = require("nodemailer");
+const bcrypt = require("bcrypt");
 
 const getlanding = (req, res) => {
   try {
@@ -10,14 +10,6 @@ const getlanding = (req, res) => {
     console.log("err");
   }
 };
-
-let transporter = nodemailer.createTransport({
-  host: "smtp=mail.gmail.com",
-  auth: {
-    user: process.env.AUTH_EMAIL,
-    pass: process.env.AUTH_PASS,
-  },
-});
 
 const getlogin = (req, res) => {
   try {
@@ -33,75 +25,93 @@ const getsignup = (req, res) => {
     console.log("err");
   }
 };
-const postsignup = async (req, res) => {
-  try {
 
-    const data = {
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      confirmPassword:req.body.confirmPassword,
-      
-    };
+const postsignup = (req, res) => {
+  let { name, email, password, confrimpassword } = req.body;
+  name = name.trim();
+  email = email.trim();
+  password = password.trim();
+  confrimpassword = confrimpassword.trim();
 
-
-    console.log(data);
-    await User.insertMany([data]);
-    if (!req.session.id) {
-      res.render("user/landingpage");
-    } else {
-      res.redirect("/");
-    }
-    
-  } catch (error) {
-    console.log(error);
-  }
-};
- 
-
-const sendOTPverificationEmail = async ({ name, email }, res) => {
-  try {
-    const otp = `${Math.floor(1000 + Math.random() * 9000)}`;
-
-    const mailoption = {
-      from: process.env.email,
-      to: email,
-      subject: "verify you email",
-      // html: (
-      //   <p>
-      //     enter ${otp}<b> in the app to verify your email address to complete the
-      //     signup</b>
-      //   </p>
-      // ),
-    };
-
-    const saltRounds = 10;
-    const hashedOTP = await bcrypt.hash(otp, saltRounds);
-    new UserOTPverification({
-      userID : __dirname,
-      otp : hashedotp,
-      createdAt:Date.now(),
-      expireAt: Date.now()+360000,
-    })
-
-    await newOTPverification.save();
-    await transporter.sendEMail(mailoption);
-    res.json({
-      status:"PENDING",
-      message:"verification otp email sent",
-      data:{
-        userID:__dirname,
-        email,
-      }
-    })
-  
-  } catch (error) {
+  if(name == ""|| email == ""|| password == "" ||confrimpassword == ""){
     res.json({
       status:"failed",
-      message:error.message
-    })
+      message:"empty input fields!"
+    });
+  }else if (!/^[a-zA-Z]*test(name)) {
+    res.json({
+      status:"failed",
+      message:"Invaild name entered"
+    });
   }
-};
+}
+
+//   try {
+
+//     const data = {
+//       name: req.body.name,
+//       email: req.body.email,
+//       password: req.body.password,
+//       confirmPassword:req.body.confirmPassword,
+
+//     };
+
+//     console.log(data);
+//     await User.insertMany([data]);
+//     if (!req.session.id) {
+//       res.render("user/landingpage");
+//     } else {
+//       res.redirect("/");
+//     }
+
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// const sendOTPverificationEmail = async ({ name, email }, res) => {
+//   try {
+//     const otp = `${Math.floor(1000 + Math.random() * 9000)}`;
+
+//     const mailoption = {
+//       from: process.env.email,
+//       to: email,
+//       subject: "verify you email",
+//       // html: (
+//       //   <p>
+//       //     enter ${otp}<b> in the app to verify your email address to complete the
+//       //     signup</b>
+//       //   </p>
+//       // ),
+//     };
+
+//     const saltRounds = 10;
+//     const hashedOTP = await bcrypt.hash(otp, saltRounds);
+//     new UserOTPverification({
+//       name : __dirname,
+//       otp : hashedotp,
+//       createdAt:Date.now(),
+//       expireAt: Date.now()+360000,
+//     })
+//     await newOTPverification.save();
+//     await transporter.sendMail(mailOption);
+//     res.json({
+
+//       status:"PENDING",
+//       message:"verification otp email sent",
+//       data:{
+//         name:__dirname,
+//         email,
+//       }
+//     })
+
+//   } catch (error) {
+//     res.json({
+//       status:"failed",
+//       message:error.message
+//     })
+//   }
+// };
 
 // Route to handle OTP validation
 // const verifyotp = ( (req, res) => {
@@ -153,5 +163,4 @@ module.exports = {
   getwishlist,
   getsignup,
   postsignup,
-
 };
